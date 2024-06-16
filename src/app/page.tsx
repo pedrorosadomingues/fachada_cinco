8dfc397e22ac6db393f8aee2c648c85d01d4dc7a
Pedro Rosa <pedrorosadomingues@gmail.com>
2024-06-16 18:09:13 -0300

    feat: Revert changes to Image component in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import ThreeSixtyViewer from "../pages/ThreeSixtyViewer";
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Home() {
   function handleClick() {
@@ -11,14 +11,14 @@ export default function Home() {
   return (
     <main className="flex items-center justfy-center relative">
       <ThreeSixtyViewer />
-      <Image
+      {/* <Image
         src="/fullscreen.svg"
         alt="Fullscreen-icon"
         width={50}
         height={48}
         className="fixed bottom-0 right-0 cursor-pointer"
         onClick={handleClick}
-      />
+      /> */}
     </main>
   );
 }
